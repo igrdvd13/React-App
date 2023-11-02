30d74303b720199605f3bb7f8ef175f148883c7c
igor <igordavyd13@gmail.com>
2023-11-01 20:24:40 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      category: '',
+      quantity: '',
+      price: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    const inputStyle = {
+      marginBottom: '10px',
+      padding: '10px',
+      backgroundColor: '#f0f8ff'
+    };
+
+    const appStyle = {
+      backgroundColor: '#d3d3d3',
+      padding: '20px'
+    };
+
+    const buttonStyle = {
+      marginRight: '10px',
+      padding: '10px',
+      backgroundColor: '#4682b4', // SteelBlue color for the buttons
+      color: 'white', // Text color for the buttons
+      border: 'none', // Remove border from the buttons
+      cursor: 'pointer' // Change cursor to pointer when hovering over the buttons
+    };
+
+    return (
+      <div className="App" style={appStyle}>
+        <h1>New Product</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div style={inputStyle}>
+            <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+          </div>
+          <div style={inputStyle}>
+            <input type="text" name="description" placeholder="Description" onChange={this.handleChange} />
+          </div>
+          <div style={inputStyle}>
+            <input type="text" name="category" placeholder="Category" onChange={this.handleChange} />
+          </div>
+          <div style={inputStyle}>
+            <input type="number" name="quantity" placeholder="Quantity" onChange={this.handleChange} />
+          </div>
+          <div style={inputStyle}>
+            <input type="number" name="price" placeholder="Price" onChange={this.handleChange} />
+          </div>
+          <button type="submit" style={buttonStyle}>Submit</button>
+          <button type="reset" style={buttonStyle}>Cancel</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
